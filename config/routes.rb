9004dba04ac14c71807037b5b6b58e72114acc52
Kevin Cuadros <kev040798@gmail.com>
2021-07-11 13:31:41 -0400

    New root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'weather#index'
+  root 'charts#index'
 
   get '/austin/temperature', to: 'weather#index'
+  get '/charts', to: 'charts#index'
 end
