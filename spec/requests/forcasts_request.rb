af925b911fec729d8a7449e1ad71b89847d8ef68
Kevin Cuadros <kev040798@gmail.com>
2021-07-12 19:19:53 -0400

    Create tests for forcast api with vcr

---
+++ spec/requests/forcasts_request.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe 'Forcasts API' do
+  describe 'Current Austin Weather' do
+    it 'should retreive current weather', :vcr do
+      get '/forcasts'
+
+      expect(response).to be_successful
+
+      json = JSON.parse(response.body, symbolize_names: true)[:data]
+
+      json.each do |data|
+        expect(data).to have_key(:id)
+        expect(data[:id]).to eq(nil)
+
+        expect(data).to have_key(:type)
+        expect(data[:type]).to eq('forcast')
+
+        expect(data).to have_key(:attributes)
+        expect(data[:attributes].keys).to eq(%i[date hourly_temps])
+
+        expect(data[:attributes][:date]).to be_a String
+        expect(data[:attributes][:hourly_temps]).to be_a Hash
+      end
+    end
+  end
+end
