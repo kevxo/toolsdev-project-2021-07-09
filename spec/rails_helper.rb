8ac3aa9b3fdaea33c6a66d70b9ab480798b0c256
Kevin Cuadros <kev040798@gmail.com>
2021-07-18 23:26:43 -0400

    Delete uneccessary config and travis script

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -77,7 +77,6 @@ end
 
 VCR.configure do |config|
   config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
-  config.allow_http_connections_when_no_cassette = true
   config.hook_into :webmock
   config.filter_sensitive_data('KEY') { ENV['KEY'] }
   config.default_cassette_options = { re_record_interval: 1.days }
